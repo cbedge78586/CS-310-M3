595fb6275771f6998734e18e433212737a6cf8ca
Charles Edge <cbedge@my365.bellevue.edu>
2025-09-23 05:58:47 -0500

    Add files via upload

---
+++ Edge - M3 Discussion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Initialize main program
+int main() {
+    int age;
+// User inputs age    
+        cout << "Please enter your age: ";
+// Age stored as age variable
+    cin >> age;
+// If age is 18 or more, output adult message
+    1f (age >= 18) {
+        cout << "You entered " << age << " and are an adult." << endl;
+    }
+// If age is less than 18, output minor message
+    else; {
+        cout << "You entered " << age << " and are a minor." << endl;
+    }
+
+    return 0;
+}
